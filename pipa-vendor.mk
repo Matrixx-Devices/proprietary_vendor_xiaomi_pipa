e46dc7bf25215391e50c2451ae91d014413e98d4
CuriousNom <aryanbiswas7585@gmail.com>
2025-05-20 18:47:05 +0530

    pipa: Import libtriplecam_video_optical_zoom prebuilt
    * kanged from alioth, needed for leica

--- pipa-vendor.mk
+++ pipa-vendor.mk
@@ -386,6 +386,7 @@ PRODUCT_PACKAGES += \
     libthirdpartpkg \
     libthreadutils \
     libtriplecam_optical_zoom_control \
+    libtriplecam_video_optical_zoom \
     libultrasound \
     libvideofilter_only \
     libvidhance \
